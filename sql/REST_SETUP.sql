b93963db52144252d310b3d8af910ef5304f41e8
Ulf Hellström <ext.ulf.hellstrom@ehalsomyndigheten.se>
2016-12-20 11:18:40 +0100

    Removed autonomous transaction from script

--- sql/REST_SETUP.sql
+++ sql/REST_SETUP.sql
@@ -2,15 +2,13 @@
   RUN THIS SCRIPT CONNECTED AS REST_DATA SCHEMA
 */
 
-DECLARE
-  PRAGMA AUTONOMOUS_TRANSACTION;
 BEGIN
 
     ORDS.ENABLE_SCHEMA(p_enabled => TRUE,
                        p_schema => 'REST_DATA',
                        p_url_mapping_type => 'BASE_PATH',
                        p_url_mapping_pattern => 'rest_data',
-                       p_auto_rest_auth => TRUE);
+                       p_auto_rest_auth => FALSE);
     
     commit;
 
