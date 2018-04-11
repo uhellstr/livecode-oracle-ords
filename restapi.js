aa71856b687de08c4f610574e2a109480fa111e2
Ulf Hellstrom <ext.ulf.hellstrom@ehalsomyndigheten.se>
2018-04-11 14:05:51 +0200

    Adding javascript restcall to livecode PL/SQL api thru ORDS

--- restapi.js
+++ restapi.js
@@ -2,9 +2,10 @@
   Javascript API to be added to the REST.html file
 */
 <script  type='text/javascript'>
+
     function countryNames() {
         var request = new XMLHttpRequest();
-        var tURL = "http://localhost:8888/ords/orclpdb1/apex/rest_data/testmodule/countrynames/";
+        var tURL = "http://localhost:8080/ords/orclpdb1/apex/rest_data/testmodule/countrynames/";
         request.open('GET', tURL, false);  // `false` makes the request synchronous
         request.send(null);
 
@@ -20,7 +21,7 @@
 
     function countryData(pCountryName) {
         var request = new XMLHttpRequest();
-        var tURL = "http://localhost:8888/ords/orclpdb1/apex/rest_data/testmodule/country/" + pCountryName;
+        var tURL = "http://localhost:8080/ords/orclpdb1/apex/rest_data/testmodule/country/" + pCountryName;
         request.open('GET', tURL, false);  // `false` makes the request synchronous
         request.send(null);
 
@@ -34,9 +35,25 @@
         return false;
     };
 
+    function plotLiveCode(pCountryName) {
+      var request = new XMLHttpRequest();
+      var tURL = "http://localhost:8080/ords/orclpdb1/apex/rest_data/testmodule/graphlivecode/" + pCountryName;
+      request.open('GET', tURL, false);  // `false` makes the request synchronous
+      request.send(null);
+
+      //alert(request.responseText);
+
+      if (request.status === 200) {
+          console.log(request.responseText);
+          return request.responseText;
+      };
+
+      return false;
+    };
+
     function plotData(pdata) {
       var data = [pdata];
       Plotly.newPlot('myDiv', data);
-    }
+    };
 
 </script>
